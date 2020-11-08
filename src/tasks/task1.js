ac923fe4a68dd3ba1d7f582c791d35d7656e4043
Roman-Kirnos <kirnos007@gmail.com>
2020-11-08 08:54:17 +0200

    first add

---
+++ src/tasks/task1.js
@@ -0,0 +1,18 @@
+const QUANTITY = 'quantity';
+const PRICE = 'price';
+const PRICE_FOR_PAIR = 'priceForPair';
+
+module.exports = (arrayClothes, param, value) => {
+  const result = [];
+
+  arrayClothes.forEach(clothes => {
+    if (param in clothes && clothes[param] === value) result.push(clothes);
+    else if (param === QUANTITY && value === 0) result.push(clothes);
+    else if (param === PRICE_FOR_PAIR && clothes.price === value)
+      result.push(clothes);
+    else if (param === PRICE && clothes.priceForPair === value)
+      result.push(clothes);
+  });
+
+  return result;
+};
