f886e1c979188de59a64ab80c11a27ce8875da9e
Roman-Kirnos <kirnos007@gmail.com>
2020-11-10 20:11:02 +0200

    work

--- src/app.js
+++ src/app.js
@@ -1,19 +1,3 @@
-// const inputData = require('./inputData.json');
-
-// const { task1: sort, task2: biggestPrice, task3: modify } = require('./tasks');
-
-// function boot(data, param, value) {
-//   const sortedArray = sort(data, param, value);
-//   console.log(sortedArray);
-
-//   const totalArray = modify(sortedArray);
-//   console.log(totalArray);
-
-//   console.log(biggestPrice);
-// }
-
-// boot(inputData, 'price', '$6');
-
 const http = require('http');
 
 const requestsHandler = require('./requestHandler');
