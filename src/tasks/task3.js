ce6d1cca047377b865d1cf445d044971cb6ad066
Roman-Kirnos <kirnos007@gmail.com>
2020-11-10 20:02:11 +0200

    done

--- src/tasks/task3.js
+++ src/tasks/task3.js
@@ -1,44 +1,13 @@
-/* eslint-disable no-nested-ternary */
-const namesObjectData = {
-  QUANTITY: 'quantity',
-  PRICE: 'price',
-  PRICE_FOR_PAIR: 'priceForPair',
-};
-
 module.exports = arrayClothes => {
   const result = arrayClothes.map(clothes => {
-    Object.values(namesObjectData).forEach(type => {
-      if (!(type in clothes)) {
-        return {
-          ...clothes,
-          [type]:
-            type === namesObjectData.PRICE
-              ? clothes[namesObjectData.PRICE_FOR_PAIR]
-              : type === namesObjectData.PRICE_FOR_PAIR
-              ? `$${clothes[namesObjectData.PRICE].split('$')[1] * 2}`
-              : 0,
-        };
+    const { price, priceForPair, quantity } = clothes;
 
-        // if (type === namesObjectData.PRICE) {
-        //   return {
-        //     ...clothes,
-        //     [type]: clothes[namesObjectData.PRICE_FOR_PAIR],
-        //   };
-        // }
-        // if (type === namesObjectData.PRICE_FOR_PAIR) {
-        //   return {
-        //     ...clothes,
-        //     [type]: `$${clothes[namesObjectData.PRICE].split('$')[1] * 2}`,
-        //   };
-        // }
-        // if (type === namesObjectData.QUANTITY) {
-        //   return {
-        //     ...clothes,
-        //     [type]: 0,
-        //   };
-        // }
-      }
-    });
+    return {
+      ...clothes,
+      quantity: quantity || 0,
+      price: price || clothes.priceForPair,
+      priceForPair: priceForPair || `$${clothes.price.split('$')[1] * 2}`,
+    };
   });
 
   return result;
