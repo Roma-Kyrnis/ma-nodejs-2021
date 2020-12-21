9149a5499ea249c3e1f8a3ccb5c1ffcbe990a186
Roman-Kirnos <kirnos007@gmail.com>
2020-12-21 13:26:23 +0200

    merge with homework-07 local

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -5,10 +5,6 @@ const controller = require('../controllers');
 const products = require('./products');
 const upload = require('./upload');
 
-router.get('/task1', controller.tasks.functionOne);
-router.get('/task2', controller.tasks.functionTwo);
-router.get('/task3', controller.tasks.functionThree);
-
 router.post('/setDataGlobal', controller.configureStore.setDataGlobal);
 router.post('/writeDataInFile', controller.configureStore.writeDataInFile);
 
