87a3b662b44b0b78d146cf66edbf067d72e4979f
Roman-Kirnos <kirnos007@gmail.com>
2020-12-24 21:04:43 +0200

    delete not needed require and delete createTables

--- src/db/knex/index.js
+++ src/db/knex/index.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-restricted-syntax */
 const Knex = require('knex');
 
-const {
-  tables: { PRODUCTS, TYPES, COLORS },
-} = require('../../config');
 const { throwIfInvalid } = require('../../utils');
 
 const dbProducts = require('./products');
@@ -18,54 +15,7 @@ async function createDBIfNotExists() {
     await knex.raw(`SELECT 'CREATE DATABASE ${database}'
       WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${database}')`);
 
-    const hasTableTypes = await knex.schema.hasTable(TYPES);
-    if (!hasTableTypes) {
-      await knex.schema.createTable(TYPES, table => {
-        table.specificType(
-          'id',
-          'INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY',
-        );
-        table.string('type').notNullable();
-        table.unique('type');
-        table.timestamps();
-        table.timestamp('deleted_at').defaultTo(null);
-      });
-    }
-
-    const hasTableColors = await knex.schema.hasTable(COLORS);
-    if (!hasTableColors) {
-      await knex.schema.createTable(COLORS, table => {
-        table.specificType(
-          'id',
-          'INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY',
-        );
-        table.string('color').notNullable();
-        table.unique('color');
-        table.timestamps();
-        table.timestamp('deleted_at').defaultTo(null);
-      });
-    }
-
-    const hasTableProducts = await knex.schema.hasTable(PRODUCTS);
-    if (!hasTableProducts) {
-      await knex.schema.createTable(PRODUCTS, table => {
-        table.specificType(
-          'id',
-          'INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY',
-        );
-        table.integer('typeId').references('id').inTable('types').notNullable();
-        table
-          .integer('colorId')
-          .references('id')
-          .inTable('colors')
-          .notNullable();
-        table.decimal('price').nullable().defaultTo(0.0);
-        table.unique(['typeId', 'colorId', 'price']);
-        table.integer('quantity').notNullable().defaultTo(1);
-        table.timestamp('deleted_at').nullable().defaultTo(null);
-        table.timestamps();
-      });
-    }
+    return true;
   } catch (err) {
     console.error(err.message || err);
     throw err;
