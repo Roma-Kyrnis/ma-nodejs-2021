6b3a9cf28d6cf107651fcfb4839aa0b812b7e76f
Roman-Kirnos <kirnos007@gmail.com>
2020-12-28 12:25:23 +0200

    merge from homework-07

--- src/server/router/upload.js
+++ src/server/router/upload.js
@@ -2,9 +2,9 @@ const router = require('@awaitjs/express').Router();
 
 const { uploads } = require('../controllers');
 
-router.postAsync('/upload', uploads.writeAsyncInFile);
-router.getAsync('/upload/filenames', uploads.filenames);
-router.postAsync('/upload/optimization/:filename', uploads.optimization);
-router.postAsync('/upload/toDB', uploads.writeAsyncInDB);
+router.postAsync('/', uploads.writeAsyncInFile);
+router.getAsync('/filenames', uploads.filenames);
+router.postAsync('/optimization/:filename', uploads.optimization);
+router.postAsync('/toDB', uploads.writeAsyncInDB);
 
 module.exports = router;
