01ff43820cdc7406660921aa3d2430cf8253f581
Roman-Kirnos <kirnos007@gmail.com>
2021-01-08 10:57:00 +0200

    fix router bug with product/:id

--- src/server/router/products/index.js
+++ src/server/router/products/index.js
@@ -7,16 +7,16 @@ const types = require('./types');
 const colors = require('./colors');
 const orders = require('./orders');
 
-router.postAsync('/', products.createProduct);
-router.getAsync('/', products.getAllProducts);
-router.getAsync('/:id', products.getProduct);
-router.patchAsync('/:id', products.updateProduct);
-router.deleteAsync('/:id', products.deleteProduct);
-
 router.use('/discounts', discounts);
 
 router.use('/types', types);
 router.use('/colors', colors);
 router.use('/orders', orders);
+
+router.postAsync('/', products.createProduct);
+router.getAsync('/', products.getAllProducts);
+router.getAsync('/:id', products.getProduct);
+router.patchAsync('/:id', products.updateProduct);
+router.deleteAsync('/:id', products.deleteProduct);
 
 module.exports = router;
