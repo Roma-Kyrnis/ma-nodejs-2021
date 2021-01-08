a6cea386c386fa74bbe6b4838d466bb86455aa05
Roman-Kirnos <kirnos007@gmail.com>
2021-01-08 11:31:10 +0200

    refactor and fix authorization

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,8 +4,9 @@ const scheduler = require('./scheduler');
 const store = require('./store');
 const createCSVToDB = require('./csvToDB');
 const throwIfInvalid = require('./throwIfInvalid');
-const createAdminHash = require('./createAdminHash')
+const createAdminHash = require('./createAdminHash');
 const authorizationTokens = require('./authorizationTokens');
+const getAuthToken = require('./getAuthToken');
 
 module.exports = {
   createCsvToJson,
@@ -16,4 +17,5 @@ module.exports = {
   throwIfInvalid,
   createAdminHash,
   authorizationTokens,
+  getAuthToken,
 };
